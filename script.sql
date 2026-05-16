e61daa307fe1d26c68b909582f7fd2349c50fef9
Hassan <hachehri@gmail.com>
2026-05-16 23:54:58 +0200

    chnage script sql

--- script.sql
+++ script.sql
@@ -8,6 +8,7 @@ use botola_maghribiya;
 
 
 -- tables users
+
 CREATE TABLE admin(
 
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,8 +25,8 @@ CREATE TABLE admin(
 
 
 -- table articles
-CREATE TABLE articles (
 
+CREATE TABLE articles (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   content TEXT NOT NULL,
@@ -33,9 +34,10 @@ CREATE TABLE articles (
   category VARCHAR(50),
   user_id int not null, 
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES admin(id) ON DELETE CASCADE
 
 ) COMMENT = 'Table the articles';
+
 
 
 CREATE TABLE comments (
@@ -51,10 +53,9 @@ CREATE TABLE comments (
 
 
 
-INSERT INTO articles (title, author, content, image, category, user_id) VALUES 
+INSERT INTO articles (title,  content, image, category, user_id) VALUES 
 (
     'التعادل السلبي يحسم مواجهة اتحاد طنجة والجيش الملكي بملعب طنجة الكبير',
-    'Hassan',
     'حسم التعادل السلبي نتيجة المباراة التي جمعت نادي اتحاد طنجة بضيفه الجيش الملكي، مساء يومه الأحد، على أرضية ملعب طنجة الكبير، ضمن منافسات الجولة 18 من البطولة الاحترافية. وانتهى الشوط الأول على نتيجة البياض رغم المحاولات المتبادلة من الجانبين، حيث سعى كل طرف لافتتاح حصة التسجيل مبكرا وأخذ الأسبقية، غير أن الحذر الدفاعي طغى على معظم فترات هذا الشوط. وخلال الشوط الثاني، ارتفع نسق اللقاء، وشهد محاولات جادة من الطرفين لفك شفرة الدفاع، غير أن تألق حارسي الفريقين ويقظتهما حالا دون بلوغ أي طرف الشباك، لتنتهي المباراة بنتيجة البياض 0-0.',
     'tanger.jpeg',
     'news',
@@ -62,7 +63,6 @@ INSERT INTO articles (title, author, content, image, category, user_id) VALUES
 ),
 (
     'الرجاء الرياضي يهزم المغرب الفاسي بثنائية وينتزع صدارة البطولة مؤقتا',
-    'Hassan',
     'نجح فريق الرجاء الرياضي في العودة بانتصار ثمين من قلب فاس، عقب تغلبه على مضيفه المغرب الرياضي الفاسي بهدفين دون رد، في المواجهة التي جمعتهما مساء يومه الأربعاء على أرضية المركب الرياضي بفاس، لحساب منافسات الجولة التاسعة عشرة من البطولة الاحترافية. وأنهى الفريق الأخضر الفصل الأول من اللقاء متفوقا بهدف نظيف وقعه المهاجم النيجيري ماتياس أوييووسي عند الدقيقة 19.',
     'A1.jpg',
     'news',
@@ -70,7 +70,6 @@ INSERT INTO articles (title, author, content, image, category, user_id) VALUES
 ),
 (
     'فوز مثير للجيش أمام حسنية أكادير',
-    'Hassan',
     'حقق الجيش الملكي فوزا مثيرا على حساب مضيفه حسنية أكادير بثلاثة أهداف لهدفين، في المباراة التي جمعتهما مساء يوم الأحد على أرضية ملعب أدرار، لحساب الجولة 20 من منافسات البطولة الاحترافية. ودخل الفريق العسكري المباراة بقوة، حيث افتتح جلال الدين الخفيف التسجيل مبكرا في الدقيقة السابعة، مانحا التقدم للضيوف منذ البداية.',
     'A2.jpg',
     'news',
@@ -78,7 +77,6 @@ INSERT INTO articles (title, author, content, image, category, user_id) VALUES
 ),
 (
     'التعادل الإيجابي يحسم مواجهة آسفي والفتح',
-    'Hassan',
     'حسم التعادل الإيجابي مواجهة أولمبيك آسفي وضيفه الفتح الرياضي، بهدف لمثله، في المباراة التي جمعتهما مساء اليوم الإثنين، على أرضية ملعب المسيرة بمدينة آسفي، لحساب الجولة 20 من البطولة الاحترافية. وأنهى أولمبيك آسفي الشوط الأول متقدما بهدف نظيف، سجله اللاعب فراجي كرمون في الدقيقة 28 عن طريق ضربة جزاء.',
     'A3.jpeg',
     'news',
@@ -94,7 +92,6 @@ INSERT INTO articles (title, author, content, image, category, user_id) VALUES
 ),
 (
     'البطولة الاحترافية تشهد منافسة قوية هذا الموسم',
-    'Hassan',
     'تواصل البطولة الاحترافية المغربية جذب اهتمام الجماهير هذا الموسم بفضل المباريات القوية والمنافسة الكبيرة بين الأندية على صدارة الترتيب. وشهدت الجولات الأخيرة ارتفاع مستوى الأداء داخل أرضية الملعب، حيث أصبحت الفرق تعتمد على أساليب لعب حديثة وتنظيم تكتيكي واضح.',
     'botola-maroc.jpg',
     'news',
@@ -109,7 +106,9 @@ VALUES (
 '1234'
 );
 
-select * from users;
 select * from articles;
+
+select * from admin;
+
 select * from comments;
 
